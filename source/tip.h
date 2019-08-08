1519092fa70c9e27081cb569480528d8c5abdb2e
simonvanbernem <10748498+simonvanbernem@users.noreply.github.com>
2019-08-08 10:25:55 +0200

    Included License in the tip.h source file

--- source/tip.h
+++ source/tip.h
@@ -1,3 +1,5 @@
+//tip is licensed under the MIT License. The full license agreement can be found at the end of this file.
+
 //flags you can set:
 // #define TIP_USE_RDTSC
 // #define TIP_WINDOWS
@@ -1602,3 +1604,27 @@ namespace tip_file_format_tcb3{
 
 }
 #endif
+
+
+
+// MIT License
+// 
+// Copyright (c) 2018 Simon van Bernem
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
